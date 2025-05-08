ef7d90df8b4f330d25664d445acace4e31f3a1a3
ktoyou <danilkakuliew@gmail.com>
2025-05-09 00:56:56 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 443,
-    https: {
-      key: fs.readFileSync('/etc/vibecast/ssl/privkey.pem'),
-      cert: fs.readFileSync('/etc/vibecast/ssl/fullchain.pem'),
-    },
-    host: true, // чтобы был доступен извне, если нужно
+  plugins: [tailwindcss(), react(), nodePolyfills()],
+  define: {
+    
   },
 });
