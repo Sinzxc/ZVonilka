d173025b902de69a943ac8613d50236d8bf651c5
ktoyou <danilkakuliew@gmail.com>
2025-05-09 13:20:46 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     https: {
       key: fs.readFileSync('/etc/vibecast/ssl/privkey.pem'),
