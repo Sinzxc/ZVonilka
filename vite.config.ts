e96c2eac94f9672f8e3abca77c9b4982003eced2
ktoyou <danilkakuliew@gmail.com>
2025-05-09 16:22:29 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
-    
+    https: {
+      key: fs.readFileSync('/etc/vibecast/ssl/privkey.pem'),
+      cert: fs.readFileSync('/etc/vibecast/ssl/fullchain.pem'),
+    },
+    host: '0.0.0.0',
+    port: 443,
   },
 });
